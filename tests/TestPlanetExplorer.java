e69ba6fa7a3aed3128df124edb5584b798051212
somename <someemail>
2017-06-09 12:06:56 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,4 +8,12 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void test_moveForth() throws PlanetExplorerException
+	{
+		PlanetExplorer vozilo = new PlanetExplorer(3, 3, N);
+		vozilo.moveForth();
+		asserteQuals("Napred", "(3, 4, N)", vozilo.getFormatedCoordinates));
+	}
 }
